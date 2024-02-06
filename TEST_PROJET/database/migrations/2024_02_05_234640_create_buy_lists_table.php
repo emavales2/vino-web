91441f2fb23a125938460092f124236da130c90b
thomasIRA <thomas.al@live.ca>
2024-02-06 17:13:51 -0500

    voir dossier TEST_PROJET

---
+++ TEST_PROJET/database/migrations/2024_02_05_234640_create_buy_lists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buy_lists', function (Blueprint $table) {
+            $table->primary(['cellar_id', 'wine_id']);
+            $table->unsignedBigInteger('cellar_id');
+            $table->unsignedBigInteger('wine_id');
+            $table->integer('quantity');
+            $table->foreign(['cellar_id', 'wine_id'])->references(['cellar_id', 'wine_id'])->on('cellar_has_wines')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buy_lists');
+    }
+};
---
+++ TEST_PROJET/resources/js/Pages/TestPage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+<template>
+    <Head title="Test" />
+    <div class="p-4">
+        <h1 class="text-bold text-xl">Its working</h1>
+        <p>on enlevera le tailwind des configs</p>
+    </div>
+</template>
