e9f5d4a0a38e445662f083ec443085c2ebdb25ef
valeriemo <v_moreau@hotmail.com>
2024-02-15 21:56:57 -0500

    style add cellar et new editbutton

---
+++ resources/js/Components/ButtonsIcons/EditButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <button @click="addAction" type="button">
+        <!-- SVG ICI -->
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'EditButton',
+    data () {
+        return {
+            fill: '#420101',
+        }
+    },
+    mounted () {
+        switch (this.color) {
+            case 'cream':
+                this.fill = '#F2D9AF'
+                break;
+            case 'burgundy':
+                this.fill = '#2B0303'
+            default:
+                this.fill = '#420101'
+                break;
+        }
+    },
+    props:['color', 'addAction']
+}
+</script>
