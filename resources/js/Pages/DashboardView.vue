e72abe88a513c51a9b481192e398287b2653d0cf
Robson Duarte Afonso <robson.duarte@gmail.com>
2024-02-20 07:15:35 -0500

    Cellar Listing

--- resources/js/Pages/DashboardView.vue
+++ resources/js/Pages/DashboardView.vue
@@ -1,73 +1,69 @@
 <template>
-    <Head title="Dashboard" />
-          
-    <div class="bckgd">
-        
-        <main>
+  <Head title="Dashboard" />
+
+  <div class="bckgd">
+
+    <main>
+      <header>
+        <h1 class="index_title">Welcome, {{ user.first_name }}</h1>
+      </header>
+
+      <button class="button">
+        <Link :href="route('wine.create')">
+        Add a custom wine
+        </Link>
+      </button>
+
+      <ul class="column_30">
+
+        <!-- --------------- * CELLARS * ------------------ -->
+        <li>
+          <section class="column_20">
             <header>
-                <h1 class="index_title">Welcome, {{ user.first_name }}</h1>
+              <span class="row_els_apart gap_20">
+                <h2 class="disp_subtitle">my cellars</h2>
+                <div>
+                  <Link v-if="cellars.length > 3" class="lien_sml cream block-font" :href="route('cellar.index')">See more
+                  </Link>
+                </div>
+              </span>
             </header>
 
-            <button class="button">
-                <Link :href="route('wine.create')">
-                    Add a custom wine
-                </Link>
-            </button>
+            <CellarListing 
+              :cellars="cellars" 
+              :limit="3" 
+            />
 
-            <ul class="column_30">
+          </section>
+        </li>
 
-                <!-- --------------- * CELLARS * ------------------ -->
-                <li>
-                    <section class="column_20">
-                        <header>
-                            <span class="row_els_apart gap_20">
-                                <h2 class="disp_subtitle">my cellars</h2>
-                                <div>
-                                    <Link v-if="cellars.length > 3" class="lien_sml cream block-font" :href="route('cellar.index')">See more</Link>
-                                </div>
-                            </span>
-                        </header>
+        <!-- --------------- * WINES * ------------------ -->
+        <li>
+          <section class="column gap_20">
+            <header>
+              <span class="row_els_apart gap_20">
+                <h2 class="disp_subtitle">my wine collection</h2>
+                <div>
+                  <Link v-if="collection.length > 3" class="lien_sml cream block-font" :href="route('collection')">See
+                  more</Link>
+                </div>
+              </span>
+            </header>
 
-                        <ul v-if="!isEmpty(cellars)" class="grid_square_horiz">
-                            <!-- Utilisez v-for pour afficher les 3 premières caves -->
-                            <li class="grid_sq_insert" v-for="(cellar, index) in cellars.slice(0, 3)" :key="cellar.id">
-                                <CellarDisplayBox :cellar="cellar" />
-                            </li>
-                        </ul>
+            <div v-if="collection.length !== 0">
+              <ul class="wine-list">
+                <WineThumbnail v-for="(wine, i) in collection.slice(0, 3)" :key="i" :wine="wine.wine"
+                  :quantities="wine.quantities" />
+              </ul>
+            </div>
 
-                        <p class="no_show" v-else><strong>OH NO!</strong> You have no cellars yet.</p>
-                    </section>
-                </li>
+            <p class="no_show" v-else><strong>OH NO!</strong> You have no wines yet.</p>
 
-                <!-- --------------- * WINES * ------------------ -->
-                <li>
-                    <section class="column gap_20">
-                        <header>
-                            <span class="row_els_apart gap_20">
-                                <h2 class="disp_subtitle">my wine collection</h2>
-                                <div>
-                                    <Link v-if="collection.length > 3" class="lien_sml cream block-font" :href="route('collection')">See more</Link>
-                                </div>
-                            </span>
-                        </header>
-
-                        <div v-if="collection.length !== 0">
-                            <ul class="wine-list">
-                            <WineThumbnail v-for="(wine, i) in collection.slice(0, 3)"
-                                :key="i"
-                                :wine="wine.wine"
-                                :quantities="wine.quantities"
-                            />
-                            </ul>
-                        </div>
-
-                        <p class="no_show" v-else><strong>OH NO!</strong> You have no wines yet.</p>
-                   
-                    </section>
-                </li>
-            </ul>
-        </main>
-    </div>  
+          </section>
+        </li>
+      </ul>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -78,22 +74,22 @@ import { Inertia } from "@inertiajs/inertia";
 import WineThumbnail from "@/Components/WineThumbnail.vue";
 
 export default {
-    name: "DashboardView",
-    components: {
-        CellarDisplayBox,
-        Head,
-        Link,
-        WineThumbnail,
+  name: "DashboardView",
+  components: {
+    CellarDisplayBox,
+    Head,
+    Link,
+    WineThumbnail,
+  },
+  layout: MainLayout,
+  methods: {
+    logout() {
+      Inertia.post(route("logout"));
     },
-    layout: MainLayout,
-    methods: {
-        logout() {
-            Inertia.post(route("logout"));
-        },
-        isEmpty(obj) {
-            return Object.keys(obj).length === 0;
-        },
+    isEmpty(obj) {
+      return Object.keys(obj).length === 0;
     },
-    props: ['cellars', 'collection', 'user'],
+  },
+  props: ['cellars', 'collection', 'user'],
 };
 </script>
