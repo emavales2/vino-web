302c78810e71fc28b5d11d7501c1a997842c1dff
thomasira <134738954+thomasira@users.noreply.github.com>
2024-02-23 11:25:23 -0500

    clean button.css + correction traduction buy-list

--- resources/lang/en/buylist.php
+++ resources/lang/en/buylist.php
@@ -4,6 +4,6 @@ return [
   'just_delete' => 'just delete them',
   'store_in_cellar' => 'do you want to store this wine in a cellar?',
   'success_store_beg' => 'Great! you just added',
-  'success_store_mid' => 'bottle(s) in your',
+  'success_store_mid' => 'bottle(s) in',
   'success_delete' => 'you deleted this wine from your buy list!'
 ];
--- resources/lang/fr/buylist.php
+++ resources/lang/fr/buylist.php
@@ -4,6 +4,6 @@ return [
   'just_delete' => 'supprimer le vin',
   'store_in_cellar' => 'voulez-vous insérer ce vin dans un cellier?', 
   'success_store_beg' => 'Super! vous venez d\'ajouter',
-  'success_store_mid' => 'bouteille(s) dans votre',
+  'success_store_mid' => 'bouteille(s) dans',
   'success_delete' => 'vous avez supprimer ce vin de votre liste d\'achat!'
 ];
